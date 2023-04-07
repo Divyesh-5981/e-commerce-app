db006ea2a70f07f0c217e2d0be85ceefb49071f1
Divyesh-5981 <divyesh.kumbhani@zuru.com>
2023-04-07 17:36:52 +0530

    add: paypal functionality

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 
 export default class Default extends Component {
   render() {
-    return <h3>page not found</h3>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h2>
+              <span className="text-danger">page not found</span>
+            </h2>
+            <h3>
+              the requested URL{" "}
+              <span className="text-danger">{window.location.pathname}</span>{" "}
+              was not found
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
